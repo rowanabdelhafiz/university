2967587dc784e090604ad55b99b4e9eb2e52e8c9
elmoushy <seif778811@gmail.com>
2022-03-18 23:56:44 +0200

    .

--- Project dr.aymen/regist_out.php
+++ Project dr.aymen/regist_out.php
@@ -2,26 +2,16 @@
     extract($_POST);
     include"user_login_list.php";
     $pn=$list->head;
-    $file=fopen("login_information.txt","a");
-    fwrite($file, $username ."\n");
-    fclose($file);
-    $fil=fopen("login_information.txt","r");
-	$l1=fgets($fil);
-    fclose($fil);
     $k=0;
     while($pn!=NULL)
 	{
-        echo $pn->name;
-		if($l1==$pn->name)
+		if($username==$pn->name)
 		{
 			$k=1;
-            
             break;
 		}
 		$pn=$pn->next;
 	}
-    $filename='login_information.txt';
-	unlink($filename);
     if($k==0)
     {
         if($opassword==$Cpassword)
