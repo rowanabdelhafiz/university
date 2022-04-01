acd2898d59f803d5837883b7ff3d5e32ccc365bb
elmoushy <seif778811@gmail.com>
2022-04-01 16:28:28 +0200

    ..

--- Project dr.aymen/register.php
+++ Project dr.aymen/register.php
@@ -27,11 +27,10 @@
 			<select name="Type" class="input-group" style="opacity: 0.5; border-radius: 25px;">
 			<option value="1">Admin</option>
     		<option value="2">Death cases officer</option>
-    		<option value="3">Compensation cases officer</option>
-			<option value="4">signature health officer</option>
-			<option value="5">Officer family court</option>
-			<option value="6">Officer's of economic courts experts</option>
-			<option value="7">Security officer</option>
+    		<option value="3">Student</option>
+			<option value="4">Guard</option>
+			<option value="5">Doctor</option>
+			<option value="6">Secretary</option>
   			</select>
 			<div class="input-group">
 				<button name="submit" class="btn">ADD</button>
