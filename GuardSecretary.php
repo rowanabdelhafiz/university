79f5f3c629f34362859787aa2a6741a40c2bb616
afawzy210 <100765824+afawzy210@users.noreply.github.com>
2022-04-03 00:08:35 +0200

    .

--- GuardSecretary.php
+++ GuardSecretary.php
@@ -2,7 +2,74 @@
 
 class GuardSecretary
 {
-    
+    protected $numDaysWorking = -1;
+    protected $numHoursWorking = -1;
+    protected $NationalID = 0;
+    protected $Address = null;
+
+    public function getNumDaysWorking()
+    {
+        if($this->numDaysWorking > -1)
+        {
+            return $this->numDaysWorking;
+        }
+    }
+
+    public function setNumDaysWorking($nW)
+    {
+        if($nW > -1)
+        {
+            $this->numDaysWorking = $nW;
+        }
+    }
+
+    public function getNumHoursWorking()
+    {
+        if($this->numDaysWorking > -1)
+        {
+            return $this->numDaysWorking;
+        }
+    }
+
+    public function setNumDaysWorking($nW)
+    {
+        if($nW > -1)
+        {
+            $this->numDaysWorking = $nW;
+        }
+    }
+
+    public function getNumDaysWorking()
+    {
+        if($this->numDaysWorking > -1)
+        {
+            return $this->numDaysWorking;
+        }
+    }
+
+    public function setNumDaysWorking($nW)
+    {
+        if($nW > -1)
+        {
+            $this->numDaysWorking = $nW;
+        }
+    }
+
+    public function getNumDaysWorking()
+    {
+        if($this->numDaysWorking > -1)
+        {
+            return $this->numDaysWorking;
+        }
+    }
+
+    public function setNumDaysWorking($nW)
+    {
+        if($nW > -1)
+        {
+            $this->numDaysWorking = $nW;
+        }
+    }
 }
 
 ?>
