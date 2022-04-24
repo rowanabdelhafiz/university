f4d158a45fc98b447268e75a5293136964c897de
afawzy210 <100765824+afawzy210@users.noreply.github.com>
2022-04-24 05:01:54 +0200

    .

--- Project dr.aymen/RegisterDetails.php
+++ Project dr.aymen/RegisterDetails.php
@@ -4,7 +4,26 @@ include_once("InID.php");
 
 class RegisterDetails extends InID
 {
-    
+    private $rgID;
+    public $crsID;
+    public $Hr;
+    public $PriceHr;
+
+    public function getRgID()
+    {
+        if($this->rgID)
+        {
+            return $this->rgID;
+        }
+    }
+
+    public function setRgID($rgID)
+    {
+        if($rgID)
+        {
+            $this->rgID = $rgID;
+        }
+    }
 }
 
 ?>
