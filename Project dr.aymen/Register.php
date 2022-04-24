80abc999a117164d3e69c88e3e0a38ae9dc06085
afawzy210 <100765824+afawzy210@users.noreply.github.com>
2022-04-24 02:27:00 +0200

    .

--- Project dr.aymen/Register.php
+++ Project dr.aymen/Register.php
@@ -9,9 +9,63 @@ class Register extends InID
     private $totalHr;
     private $totalPriceHr; 
 
+
+    function remove_register($ID)
+    {
+        $fileObj = new filemanager();
+        $fileObj->setFilenames("Register.txt");
+        $fileObj->setSeparator("~");
+        $Rf = $fileObj->AllContents();
+        for($i = 0; $i< count($Rf); $i++)
+        {
+            $ar1 = explode($fileObj->getSeparator(),$Rf[$i]);
+
+            if($ar1[1] == $StdId)
+            {
+                $fileObj2 = new filemanager();
+                $fileObj2->setFilenames("RegisterDetails.txt");
+                $fileObj2->setSeparator("~");
+                $f2 = fopen($fileObj2->getFilenames(),"r+");
+                while(!feof($f2))
+                {
+                    $line2=fgets($f2);
+                    $ar2 = explode($fileObj2->getSeparator(),$line2);
+
+                    if($ar2[1] == $ar1[0] && $ar2[2] == $CrsId)
+                    {
+                        $ar1[3]-=$ar2[3];
+                        $ar1[4]-=$ar2[4];
+                        
+                        //function delete line in file manager from fileObj2 for RegisterDetails!!
+                        $fileObj2->remove_dataFile($line2);
+
+                        $nL="";
+                        for($i=0;$i<count($ar1);$i++)
+                        {
+                            $nL.=$ar1[$i];
+                            if($i<count($ar1)-1)
+                            {
+                                $nL.=$fileObj->getSeparator();
+                            }
+                        }
+
+                        //function update line in file manager from fileObj for RegisterDetails!!
+                        $fileObj->update_dataFile($line,$nL);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+
     public function getStID()
     {
-        return $this->StID;
+        if($this->StID > 0)
+        {
+            return $this->StID;
+        }
+        
     }
 
     /**
